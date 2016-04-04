7eec13762b21b08729e18ed05c9ad618cf4b88ed
Xiao <shengx@bu.edu>
2016-04-04 15:17:31 -0400

    Convolutional neural network with theano

--- Main_theano.py
+++ Main_theano.py
@@ -40,7 +40,41 @@ testOutput = np.transpose(testOutput)
 
 #%% deep neural network
 # reference: http://deeplearning.net/tutorial/lenet.html
-
+import numpy as np
 import theano
 from theano import tensor as T
 from theano.tensor.nnet import conv2d
+
+rng = np.random.RandomState(23455)
+
+# instantiate 4D tensor for input
+
+input = T.tensor4(name = 'input')
+
+# initialize shared variable for weights
+w_shp = (2, 3, 9, 9)
+w_bound = np.sqrt(3*9*9)
+W = theano.shared( np.asarray(rng.uniform(low = -1.0/w_bound, high = 1.0/w_bound, size = w_shp),dtype=input.dtype), name = 'W')
+
+# initalize shared variable for bias
+b_shp = (2, )
+b = theano.shared(np.asarray(rng.uniform(low = -0.5, high = 0.5, size = b_shp),dtype = input.dtype), name = 'b')
+
+#build symbolic expression that compute the convolution of input with filter W
+nv_out = conv2d(input,W)
+# add bias and apply activation function
+output = T.nnet.sigmoid(conv_out + b.dimshuffle('x', 0, 'x', 'x'))
+
+# create theano function to compute filtered images
+f = theano.function([input], output)
+
+#%% apply the convolutional layer
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img = mpimg.imread('test.jpg')
+img = np.asarray(img, dtype = 'float64') /256
+
+img_ = img.transpose(2, 0 ,1).reshape(1, 3, 500, 422)
+
+filtered_img = f(img_)
